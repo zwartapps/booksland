521e62855c6e07fc817cb32385a6d325d2e444ab
zwartapps <zwart.apps@gmail.com>
2021-02-14 14:42:13 +0000

    ES6

--- api/index.js
+++ api/index.js
@@ -1,10 +1,11 @@
 "use strict";
 
-var mongoose = require('mongoose');
+//Converted var mongoose = require ('mongoose') to ES6 code 
+import { Promise, connect } from 'mongoose';
 
-mongoose.Promise = global.Promise;
+Promise = global.Promise;
 //new url parser added, new Server Discover and Monitoring engine added
-mongoose.connect('mongodb://localhost:27017/booksland', { useNewUrlParser: true , useUnifiedTopology: true })
+connect('mongodb://localhost:27017/booksland', { useNewUrlParser: true , useUnifiedTopology: true })
     .then(() => {
         console.log("conexion a BD booksland se ha realizado correctamente!!")
     }).catch(err => console.log(err));
